6e90feb975b6ae860b794d278a8487f33a649fe5
Cyrille Djoko Djilo <cyrilledjoko@gmail.com>
2019-08-19 21:02:18 -0400

    Object literal improvements

--- src/app.ts
+++ src/app.ts
@@ -1,26 +1,47 @@
-const pizzas = [
-    { name: 'Pepperoni', toppings: ['pepperoni'] }
-];
+// const pizzas = [
+//     { name: 'Pepperoni', toppings: ['pepperoni'] }
+// ];
 
-const mappedPizzas = pizzas.map(pizza => pizza.name.toUpperCase());
+// const mappedPizzas = pizzas.map(pizza => pizza.name.toUpperCase());
 
-console.log(mappedPizzas);
+// console.log(mappedPizzas);
+
+// const pizza = {
+//     name: 'Balzing inferno',
+//     getName: function () {
+//         console.log(this.name);
+//     },
+//     getName2: () => pizza.name
+// }
+
+// pizza.getName()
+// console.log(pizza.getName2())
+
+// function multiply(a: number, b = 25) {
+//     return a * b;
+// }
+
+
+// console.log(multiply(5));
+// console.log(multiply(5, 35));
 
 const pizza = {
-    name: 'Balzing inferno',
-    getName: function () {
-        console.log(this.name);
-    },
-    getName2: () => pizza.name
+    name: 'Pepperoni',
+    price: 15,
+    getName() {
+        return pizza.name;
+    }
+},
+    toppings = ['pepperoni'],
+    order = { pizza, toppings };
+
+console.log(order);
+
+console.log(pizza.getName());
+
+
+function createOrder(pizza: any, toppings: any) {
+    return { pizza, toppings };
 }
 
-pizza.getName()
-console.log(pizza.getName2())
-
-function multiply(a: number, b = 25) {
-    return a * b;
-}
-
-
-console.log(multiply(5));
-console.log(multiply(5, 35));
+console.log(createOrder(pizza, toppings));
