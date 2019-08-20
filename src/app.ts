affd526f28429abe849868ffb947adc62d053626
Cyrille Djoko Djilo <cyrilledjoko@gmail.com>
2019-08-19 21:08:35 -0400

    Rest parameters

--- src/app.ts
+++ src/app.ts
@@ -25,23 +25,35 @@
 // console.log(multiply(5));
 // console.log(multiply(5, 35));
 
-const pizza = {
-    name: 'Pepperoni',
-    price: 15,
-    getName() {
-        return pizza.name;
-    }
-},
-    toppings = ['pepperoni'],
-    order = { pizza, toppings };
+// const pizza = {
+//     name: 'Pepperoni',
+//     price: 15,
+//     getName() {
+//         return pizza.name;
+//     }
+// },
+//     toppings = ['pepperoni'],
+//     order = { pizza, toppings };
 
-console.log(order);
+// console.log(order);
 
-console.log(pizza.getName());
+// console.log(pizza.getName());
 
 
-function createOrder(pizza: any, toppings: any) {
-    return { pizza, toppings };
+// function createOrder(pizza: any, toppings: any) {
+//     return { pizza, toppings };
+// }
+
+// console.log(createOrder(pizza, toppings));
+
+// function sumAll(arr: number[]) {
+//     return arr.reduce((prev, next) => prev + next);
+// }
+// console.log(sumAll([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]));
+
+function sumAll(message: string, ...arr: number[]) {
+    console.log(arguments);
+    console.log(message);
+    return arr.reduce((prev, next) => prev + next);
 }
-
-console.log(createOrder(pizza, toppings));
+console.log(sumAll('Yo!', 1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
